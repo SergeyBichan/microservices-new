3865dcb1bec111712fe1cf669ab26e9092255edf
SergeyBichan <ugidancer@gmail.com>
2023-06-08 10:17:32 +0300

    try github

---
+++ product-service/src/main/java/com/programmingtechnie/productservice/ProductServiceApplication.java
@@ -0,0 +1,14 @@
+package com.programmingtechnie.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+}
